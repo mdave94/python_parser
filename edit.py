c907c76d089ca725be6599d2d43f3fa6db0d6a46
mdave94 <molnardavid2002@gmail.com>
2022-05-09 13:49:02 +0200

    update

--- edit.py
+++ edit.py
@@ -6,9 +6,11 @@ from openpyxl.styles import Alignment
 
 
 book = load_workbook('silence_labels.xlsx')
-#sheet  = book.active
-
+sheet  = book.active
+max_column = sheet.max_column
+max_row = sheet.max_row
 sheet_cells=[]
+
 """
 for row in sheet.iter_rows():
     row_cells = []
@@ -19,13 +21,32 @@ for row in sheet.iter_rows():
 """
 #print(sheet_cells)
 
-book.create_sheet('uj_tabla')
-sh1 = book['uj_tabla']
-sh1 = book.active
-
+#book.create_sheet('uj_tabla')
+##h1 = book['uj_tabla']
+#sh1 = book.active
 
 temp = []
+sheet_cells=[]
+for row in sheet.iter_rows():
+    row_cells = []
+    for cell in row:
+        row_cells.append(cell.value)
+        row_cells[0].upper()   
+        text = row_cells[0]+" ("+row_cells[1]+")\n"+row_cells[2]+","+str(row_cells[3])+"\n"+row_cells[5]+","+row_cells[4]
+    sheet_cells.append(tuple(row_cells))
+    print(text)
+
+
+
+
+
+#sheet.merge_cells('A1:G1')  
+
+
+"""
+temp = []
 for i in range(1,7):
+
     test = sh1.cell(row=1,column=i)
     temp.append(test.value)
 
@@ -42,7 +63,7 @@ targetCell = sh1.cell(row=1,column=1)
 targetCell.value = text
 
 targetCell.alignment = Alignment(horizontal='center', vertical='center')  
-
+"""
 #print(text)
  #print(type(temp))
 
@@ -53,5 +74,5 @@ targetCell.alignment = Alignment(horizontal='center', vertical='center')
  #   sh1.append(item)
 
 
-book.save('silence_labels.xlsx')
+#book.save('silence_labels.xlsx')
 print(" lefutott rendesen")
