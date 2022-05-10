05014ee91311b97be6045ff26d975cac41b05d96
mdave94 <molnardavid2002@gmail.com>
2022-05-10 13:58:26 +0200

    next lvl

--- edit.py
+++ edit.py
@@ -30,34 +30,38 @@ for row in sheet.iter_rows():
 
 temp = []
 sheet_cells=[]
-for row in sheet.iter_rows():
+for idx, row in enumerate(sheet.iter_rows()):
     row_cells = []
     for cell in row:
         row_cells.append(cell.value)
         
-  #  print(row_cells)       
+   # print(row_cells)       
     sheet_cells.append(tuple(row_cells))
     text = row_cells[0]+" ("+row_cells[1]+")\n"+row_cells[2]+","+str(row_cells[3])+"\n"+row_cells[5]+","+row_cells[4]
-   # print(text )
-   # sheet.merge_cells(start_row=row_cells[0], start_column=1, end_row=row_cells[5], end_column=4)
-    
-    temp.append(text)
+   # print(text )cls
+    sheet.merge_cells(start_row=idx+1, start_column=1, end_row=idx+1, end_column=7)
+    targetCell = sheet.cell(row=idx+1,column=1)
+    targetCell.value = text
+    #temp.append(text)
+    print(targetCell)
   #  print(text+" ||\n")
 
-print(temp)
+#print(temp)
     #sheet.append(text)
-
+""""
 for i in range(1,max_row+1):
     row_cells = []
-    for j in range(1,max_column):
+    for j in range(1,max_column+1):
         row_cells.append(cell.value)
+        print(row_cells)
         sheet_cells.append(tuple(row_cells))
     text = row_cells[0]+" ("+row_cells[1]+")\n"+row_cells[2]+","+str(row_cells[3])+"\n"+row_cells[5]+","+row_cells[4]
-    sheet.merge_cells(start_row=i, start_column=1, end_row=i, end_column=7)
+   
+    #sheet.merge_cells(start_row=i, start_column=1, end_row=i, end_column=7)
     targetCell = sheet.cell(row=i,column=i)
-    
+#print(text)  
     #targetCell.value = text
-
+"""
 
 #sheet.merge_cells('A1:G1')  
 
